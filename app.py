bae17f14734f9a8a52b23310fbeb916f4ff0fb88
DivineSaumil <86531012+DivineSaumil@users.noreply.github.com>
2024-02-26 23:20:28 +0530

    Added json API endpoint

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+servantinfo = [
+    {'name': 'Altria Pendragon', 'level': 90, 'grailed': 'Yes', 'skill_levels': '10/10/10', 'rarity': '5*'},
+    {'name': 'Gilgamesh', 'level': 90, 'grailed': 'No', 'skill_levels': '10/10/10', 'rarity': '5*'},
+    # Add more servants as needed
+]
 
 
 @app.route('/')
 def home():
-    # Example list of servants
-    servantinfo = [
-        {'name': 'Altria Pendragon', 'level': 90, 'grailed': 'Yes', 'skill_levels': '10/10/10', 'rarity': '5*'},
-        {'name': 'Gilgamesh', 'level': 90, 'grailed': 'No', 'skill_levels': '10/10/10', 'rarity': '5*'},
-        # Add more servants as needed
-    ]
     return render_template('home.html', servantinfo=servantinfo)
+
+
+@app.route('/api/servants')
+def servantdata():
+    return jsonify(servantinfo)
 
 
 if __name__ == '__main__':
